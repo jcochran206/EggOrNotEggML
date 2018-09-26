0b93b59b48293a0fab443f32f8ff99da25896b74
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-09-26 15:20:18 -0700

    added tableview delegates and connected two functions to provide rows for table

--- EggOrNotEggML/EggOrNotEggML/ViewController.swift
+++ EggOrNotEggML/EggOrNotEggML/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    //functions section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hi"
+        return cell
     }
 
 
