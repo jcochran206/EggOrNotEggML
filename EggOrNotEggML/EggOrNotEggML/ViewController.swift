1e0dca5349aed0ae455c340b5b11129a47d6c762
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-09-26 15:08:07 -0700

    design added: added uiimage view to show image and tableview section to provide predictions to user and two buttons for folder or camera use

---
+++ EggOrNotEggML/EggOrNotEggML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggOrNotEggML
+//
+//  Created by Jonathan Cochran on 9/26/18.
+//  Copyright © 2018 Jonathan Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
