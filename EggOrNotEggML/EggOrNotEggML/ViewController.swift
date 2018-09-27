1ceac9677724f39785531ea1133417cbeea6c93f
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-09-26 19:05:10 -0700

    added coreML model to use to detect images

--- EggOrNotEggML/EggOrNotEggML/ViewController.swift
+++ EggOrNotEggML/EggOrNotEggML/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var restnetModel = Resnet50()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        if let image = imageView.image { detectImage(image: image)}
     }
     
     //functions section
+    //Mark process thru image in resnet model thru a request
+    func detectImage(image:UIImage) {
+        if let model = try? VNCoreMLModel(for: restnetModel.model){
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                if let results = request.results as? [VNClassificationObservation] {
+                    print(results)
+                }
+            }
+            //converts image into data and uses handler
+            if let imageData = image.jpegData(compressionQuality: 1.0) {
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
+            
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
